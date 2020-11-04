11f4c0ce9b561381ea22fa50ab0897b7fb257a8a
Jef Oliver <jef@eljef.me>
2020-11-03 16:30:49 -0800

    Add License Headers
    Add missing license headers to scripts. This really shouldn't
    be necessary, but this world is an intellectual property heavy
    crap-shoot where everything must be guarded and permission given.
    So, yeah, adding headers to give permission. No one should get
    in trouble this way.
    
    Signed-off-by: Jef Oliver <jef@eljef.me>

--- Windows/installers/02_install_chocolatey.ps1
+++ Windows/installers/02_install_chocolatey.ps1
@@ -1,3 +1,19 @@
+# Copyright (C) 2020 Jef Oliver.
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+# SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+# IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+# Authors:
+# Jef Oliver <jef@eljef.me>
+
 Set-ExecutionPolicy Bypass -Scope Process -Force; `
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; `
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
