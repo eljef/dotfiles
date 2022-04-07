7840a1477eaa8d5a14b9321267538d6d5fa03832
Jef Oliver <jef@eljef.me>
2022-04-07 16:28:25 -0700

    fix: Update Copyright Headers
    Signed-off-by: Jef Oliver <jef@eljef.me>

--- os/windows/00_common/03_uninstallers/00_uninstall_default_applications.ps1
+++ os/windows/00_common/03_uninstallers/00_uninstall_default_applications.ps1
@@ -1,4 +1,4 @@
-# Copyright (C) 2020-2021 Jef Oliver.
+# Copyright (C) 2020-2022 Jef Oliver.
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted.
