603b46c8a00e348dd9257a9eab3247a0fd276f48
Jef Oliver <jef@eljef.me>
2023-01-23 10:31:53 -0800

    update(fonts): Use Nerd Fonts v2.3.2
    Signed-off-by: Jef Oliver <jef@eljef.me>

--- dotfiles/gui/windows/00_install_fonts.ps1
+++ dotfiles/gui/windows/00_install_fonts.ps1
@@ -14,7 +14,7 @@
 # Authors:
 # Jef Oliver <jef@eljef.me>
 
-$nfVersion = '2.3.1'
+$nfVersion = '2.3.2'
 $nfDownloadUrl = 'https://github.com/ryanoasis/nerd-fonts/releases/download'
 $nfReleaseURL = "$( $nfDownloadUrl )/v$( $nfVersion)"
 $nfZipCascadiaCode = "CascadiaCode.zip"
